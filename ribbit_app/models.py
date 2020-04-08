4cf27bac4383b79a7a6dbf55353985de3dda298b
onkar singh <singh.onkar97@gmail.com>
2020-04-08 12:33:54 -0400

    Added Forms

--- ribbit_app/models.py
+++ ribbit_app/models.py
@@ -3,14 +3,15 @@ from django.contrib.auth.models import User
 import hashlib
 
 
-
 class Ribbit(models.Model):
-    content =models.charfield(max_length=140)
-    user=models.ForeignKey(User)
-    creation_date=models.DateTimeField(auto_now=True,blank=True)
+    content = models.CharField(max_length=140)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    creation_date = models.DateTimeField(auto_now=True, blank=True)
+
+
 class UserProfile(models.Model):
-    user= models.OneToOneField(User)
-    follows= models.ManyToManyField('self',related_name='followed_by',symmetrical=False)
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    follows = models.ManyToManyField('self', related_name='followed_by', symmetrical=False)
 
     def gravatar_url(self):
         return "http://www.gravatar.com/avatar/%s?s=50" % hashlib.md5(self.user.email).hexdigest()
